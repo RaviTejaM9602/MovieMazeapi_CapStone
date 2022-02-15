0074149e85120a652a866d661755910b3bc2720a
myelin0 <emi.veisllari@gmail.com>
2022-02-15 21:55:12 +0100

    Display dynamic movies on page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-import "./style.css";
+// // import './style.css'
 
+const List = document.querySelector('#movies');
+const movies_no=10;
 
+const fetchMovies = async ()=>{
+  for (let i = 1; i <=100; i++) {
+    await getMovie(i);
+  } 
+};
+
+ const getMovie = async (id) => {
+   try{
+  const url = `https://api.tvmaze.com/shows/${id}`;
+  const res = await fetch(url);
+   const movie = await res.json();
+   console.log(movie);
+   createMovieCard(movie);
+   }catch (error){ 
+     console.error(error);
+   }
+  }   
+
+fetchMovies();
+
+function createMovieCard(movie){
+  const movieEl = document.createElement('img');
+  movieEl.setAttribute("src", `${movie.image.medium}`);
+  const pokeInnerHtml=`
+  <img src>
+  `
+  movieEl.innerHTML=pokeInnerHtml;
+  List.appendChild(movieEl)
+  console.log(movieEl);
+}
 
