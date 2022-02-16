e751f09e0c40a250d76bcc56b5ea8c27b89b33d0
myelin0 <emi.veisllari@gmail.com>
2022-02-16 01:20:43 +0100

    Dispay & style  movies likes and comment buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// // import './style.css'
+import './style.css'
 
 const List = document.querySelector('#movies');
 const movies_no=10;
 
 const fetchMovies = async ()=>{
-  for (let i = 1; i <=100; i++) {
+  for (let i = 1; i <=10; i++) {
     await getMovie(i);
   } 
 };
@@ -24,13 +24,16 @@ const fetchMovies = async ()=>{
 fetchMovies();
 
 function createMovieCard(movie){
-  const movieEl = document.createElement('img');
-  movieEl.setAttribute("src", `${movie.image.medium}`);
-  const pokeInnerHtml=`
-  <img src>
-  `
-  movieEl.innerHTML=pokeInnerHtml;
-  List.appendChild(movieEl)
-  console.log(movieEl);
+List.innerHTML += `<li class="movie">
+<img class="mov-post" src=${movie.image.medium}>
+<div class="mov-detail">
+<p class="mov-name">${movie.name}</p>
+<div class="likes">
+<h4 class="total-likes">${movie.likes}</h4>
+<i id=${movie.id} class="fas fa-heart"></i>
+</div>
+</div>
+<button type="button" id=${movie.id} class="comments">Comments</button>
+</li>`;
 }
 
